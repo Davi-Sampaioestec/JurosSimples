c29ed7a307391a42daf231ab1121ddde9b39ffff
Gabriella_Sampaio <davisampaioall@gmail.com>
2025-04-06 21:35:35 -0300

    Tarefa Finalizada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+
+Console.Write("Insira o total de capital 💰 : ");
+double	capital =  Convert.ToDouble (Console.ReadLine());
+
+Console.Write("Insira o percentual da taxa de juros % : ");
+double taxaDeJuros = Convert.ToDouble (Console.ReadLine());
+double taxaDeJuros2 = taxaDeJuros /100;
+
+Console.Write("Insira o tempo em meses ⏳ : ");
+double tempo = Convert.ToDouble (Console.ReadLine());
+
+double juros = capital * taxaDeJuros2 * tempo;
+
+double montante = capital + juros;
+
+Console.WriteLine($"Montante {montante:C}");
+
+Console.WriteLine($"Juros {juros:C}");
